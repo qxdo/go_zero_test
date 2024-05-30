b9f9f9c151a9a5d8d1bc3bb7cadc61654beaea29
qxdo <qxdo123@gmail.com>
2024-05-30 23:12:28 +0800

    update code

--- api/index.go
+++ api/index.go
@@ -2,37 +2,29 @@ package api
 
 import (
 	"fmt"
-	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/rest"
 	"go_zero_test/inner/config"
 	"go_zero_test/inner/handler"
 	"go_zero_test/inner/svc"
 	"net/http"
-	"bytes"
-	"os/exec"
 )
 
 var srv rest.Server
 
 func init() {
-	
+
 	var c config.Config
-	// 执行 ls 命令
-	cmd := exec.Command("ls", "-la") // 使用 "-la" 选项以长格式列出所有文件，包括隐藏文件
-	var out bytes.Buffer
-	cmd.Stdout = &out // 将输出重定向到 out 变量
-
-	// 运行命令
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("执行 ls 命令失败:", err)
-		return
+	//conf.MustLoad("./etc/gozerotest-api.yaml", &c)
+	c = config.Config{
+		RestConf: rest.RestConf{
+			ServiceConf: service.ServiceConf{
+				Name: "go-zero-test",
+			},
+			Host: "0.0.0.0",
+			Port: 8888,
+		},
 	}
-
-	// 输出 ls 命令的结果
-	fmt.Print(out.String())
-	
-	conf.MustLoad("./etc/gozerotest-api.yaml", &c)
 
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
